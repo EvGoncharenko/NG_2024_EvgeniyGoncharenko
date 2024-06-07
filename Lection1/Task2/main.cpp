09770c202318c6366ba9dbeb72cb7b24075315dd
EvGoncharenko <Kilimangaro99@Gmail.com>
2024-06-07 20:31:40 +0300

    Added my second task of the first lection

---
+++ Lection1/Task2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int firstnum = 0;
+    int secondnum = 0;
+
+    cout << "Your first number: " << endl;
+    cin >> firstnum;
+    cout << "Enter second neumber: " << endl;
+    cin >> secondnum;
+
+    cout << "Result: " << firstnum + secondnum << endl;
+}
