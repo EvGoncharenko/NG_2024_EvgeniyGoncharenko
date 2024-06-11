b6921a9ab9436806d8bb5a1516fd4f9755cdb7b2
EvGoncharenko <Kilimangaro99@Gmail.com>
2024-06-12 00:46:19 +0300

    Added my fourth task of my first lection

--- Lection1/Task4/main.cpp
+++ Lection1/Task4/main.cpp
@@ -6,21 +6,18 @@ using namespace std;
 int main()
 {
     char znak;
-    int fir = 0, sec = 0, num = 0;
+    int fir = 0, sec = 0;
 
     cout << "Enter sign: '+', '-', '*', '/', '2', 'V'. ";
     cin >> znak;
 
     if (znak == '+' || znak == '-' || znak == '*' || znak == '/'){
-        cout << "Enter first number: ";
-        cin >> fir;
-
-        cout << "Enter second number: ";
-        cin >> sec;
+        cout << "Enter your numbers: ";
+        cin >> fir >> sec ;
 
     } else if (znak == '2' || znak == 'V') {
         cout << "Enter your number: ";
-        cin >> num;
+        cin >> fir;
     }
 
     cout << "Your result: ";
@@ -39,10 +36,10 @@ int main()
         cout << fir / sec;
         break;
     case '2':
-        cout << pow(num,2);
+        cout << pow(fir,2);
         break;
     case 'V':
-        cout << sqrt(num);
+        cout << sqrt(fir);
         break;
     default:
         cout << "Oops. Sorry, this sign is not provided, choose any of the proposed ones.";
