cd533265d58b94112c9d8f47541d0df10d78a97d
EvGoncharenko <Kilimangaro99@Gmail.com>
2024-07-10 23:44:50 +0300

    Added my sixth task of the first lection

--- Lection1/Task6/main.cpp
+++ Lection1/Task6/main.cpp
@@ -10,7 +10,7 @@ int main(){
     cin >> num;
 
     for(int index=0; index<num+1; index++){
-        for(int inden=0; inden<num-index; inden++){
+        for(int left_indent=0; left_indent<num-index; left_indent++){
             cout << " " ;
         }
         for(int object = 0; object < 2*index-1; object++){
