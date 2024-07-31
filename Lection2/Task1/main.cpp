b740450aba31424de67040db4750d2de1afc5966
EvGoncharenko <Kilimangaro99@Gmail.com>
2024-07-31 23:33:27 +0300

    Added my first task of the second lection

--- Lection2/Task1/main.cpp
+++ Lection2/Task1/main.cpp
@@ -3,8 +3,15 @@
 using namespace std;
 
 int main(){
-    int index[10]={0, 50, 2580, 5824, 86, 2024, 8080, 55, 56985, 10};
+    int index[10];
     int inum , num = 0, sum;
+
+    for (int numbers=0; numbers<10; numbers++) {
+        cout << "Vvedite sumu na chetu: ";
+        cin >> num;
+        index[numbers] = num;
+    }
+    num = 0;
 
     while (num < 4){
         cout << "Operazyi s schetami - 1" << endl << "Informazia o schetah - 2" << endl << "Viyti iz programi - 3" << endl;
@@ -23,19 +30,29 @@ int main(){
 
                 switch (num) {
                     case 1:
-                        if(index[inum] > 0){
-                            cout << "Suma snyatia: ";
-                            cin >> num;
-                            cout << "Vashi denygi snyati, na schetu: ";
-                            index[inum] -= num;
-                        } else {
-                            cout << "Y vas net denyeg: ";
-                        }
-                        cout << index[inum] << endl << "Hotite polozyt na schet denygi? 1 - net , 2 - da" << endl;
+                    while(index[inum] > 0){
+                        cout << "Suma snyatia: ";
                         cin >> num;
-                        if(num == 1){
-                            break;
+                        if(num > 0){
+                            if(index[inum]>num){
+                                index[inum] -= num;
+                                cout << "Vashi denygi snyati, na schetu: " << index[inum]<< endl;
+                                break;
+                            } else {
+                                cout << "Error, suma snyatia bolshe sumi na schetu!!" << endl;
+                            }
+                        } else {
+                            cout << "Error, nevozmojno snyatb sumu menshe nulya!!" << endl;
                         }
+                    }
+                    if (index[inum]<=0){
+                        cout << "Y vas net denyeg: ";
+                    }
+                    cout << "Hotite polozyt na schet denygi? 1 - net , 2 - da" << endl;
+                    cin >> num;
+                    if(num == 1){
+                            break;
+                    }
                     case 2:
                         cout << "Suma dla scheta: ";
                         cin >> sum;
@@ -52,7 +69,7 @@ int main(){
                 break;
 
             case 2:
-                int sum, min = index[0], max = index[0];
+                int sum, min = index[0], max = index[0], position_min, position_max;
 
                 for(int range=0; range<10; range++){
                     sum += index[range];
@@ -64,7 +81,31 @@ int main(){
                     }
                 }
 
-                cout << "Suma schetov: " << sum << endl << "Min: " << min << endl << "Max: " << max << endl;
+                cout << "Suma schetov: " << sum << endl << "Min: " << min << endl << "Schet pod nomerom: ";
+
+                for(int search_position_min=0; search_position_min<10; search_position_min++){
+                    if(index[search_position_min]==min){
+                        position_min = search_position_min;
+                        position_min++;
+                    } else {
+                        continue;
+                    }
+                    cout << position_min << " ";
+                }
+
+                cout << endl << "Max: " << max << endl << "Schet pod nomerom: ";
+
+                for(int search_position_max=0; search_position_max<10; search_position_max++){
+                    if(index[search_position_max]==max){
+                        position_max = search_position_max;
+                        position_max++;
+                    } else {
+                        continue;
+                    }
+                    cout << position_max << " ";
+                }
+
+                cout << endl;
         }
 
         if(num==3){
